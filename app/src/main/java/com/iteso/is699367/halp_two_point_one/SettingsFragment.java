00df2fa68dafe664798e7a52e1642193d67e7612
OverripeWings3 <sjacobust@gmail.com>
2018-04-23 11:53:13 -0500

    Preferences/Strings-es

--- app/src/main/java/com/iteso/is699367/halp_two_point_one/SettingsFragment.java
+++ app/src/main/java/com/iteso/is699367/halp_two_point_one/SettingsFragment.java
@@ -17,12 +17,8 @@ public class SettingsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        Prefe
         return view;
-    }
-
-    public void setText() {
-
     }
 }
