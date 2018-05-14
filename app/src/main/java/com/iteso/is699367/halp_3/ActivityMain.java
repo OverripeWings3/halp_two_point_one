376e7e15a109262b8504e0f649d690cc34d03dbc
Jorge Perez <alberto.perezf2@gmail.com>
2018-05-13 23:40:56 -0500

    Fab commit

--- app/src/main/java/com/iteso/is699367/halp_3/ActivityMain.java
+++ app/src/main/java/com/iteso/is699367/halp_3/ActivityMain.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -87,6 +88,8 @@ public class ActivityMain extends AppCompatActivity  implements
         }
 
         onPostCreate(savedInstanceState, null);
+
+
 
     }
 
--- app/src/main/java/com/iteso/is699367/halp_3/AssigmentFragment.java
+++ app/src/main/java/com/iteso/is699367/halp_3/AssigmentFragment.java
@@ -3,6 +3,7 @@ package com.iteso.is699367.halp_3;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -10,6 +11,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -38,25 +40,37 @@ public class AssigmentFragment extends Fragment {
         // Required empty public constructor
     }
 
-
+    FloatingActionButton fab;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_assigments, container, false);
+        fab = rootView.findViewById(R.id.fab);
+
+        if (fab != null) {
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getActivity(), "fab", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+
         recyclerView = rootView.findViewById(R.id.fragment_recycler);
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser user = mAuth.getCurrentUser();
         firebaseDatabase = FirebaseDatabase.getInstance().getReference();
         mUserTasks = firebaseDatabase.child("users").child(user.getUid()).child("assignments");
+
         return rootView;
+
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
         recyclerView.setHasFixedSize(true);
         // Use a linear layout manager
         LinearLayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
@@ -97,6 +111,7 @@ public class AssigmentFragment extends Fragment {
 
 
     }
+
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
