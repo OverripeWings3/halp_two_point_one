06cf8a52dffed6f2d13861cdddfa6d1723993d51
Jorge Perez <alberto.perezf2@gmail.com>
2018-05-14 05:07:57 -0500

    add class2

--- app/src/main/java/com/iteso/is699367/halp_3/ActivityAddClass.java
+++ app/src/main/java/com/iteso/is699367/halp_3/ActivityAddClass.java
@@ -6,40 +6,45 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.iteso.is699367.halp_3.beans.Classes;
 import com.iteso.is699367.halp_3.beans.Tasks;
 
-public class ActivityAddTask extends AppCompatActivity {
-
+public class ActivityAddClass extends AppCompatActivity {
     DatabaseReference firebaseDatabase = FirebaseDatabase.getInstance().getReference();
     FirebaseAuth mAuth = FirebaseAuth.getInstance();
     FirebaseUser user = mAuth.getCurrentUser();
-    DatabaseReference mUserTasks = firebaseDatabase.child("users").child(user.getUid()).child("assignments");
+    DatabaseReference mUserClasses = firebaseDatabase.child("users").child(user.getUid()).child("classes");
 
     EditText name;
-    EditText clas;
+    EditText room;
+    EditText teacher;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_task);
+        setContentView(R.layout.activity_add_class);
 
-        Button button = findViewById(R.id.addTaskButton);
-        name = findViewById(R.id.addTaskName);
-        clas = findViewById(R.id.addTaskClass);
-        final DatePicker duedate = findViewById(R.id.addTaskDuedate);
+        button = findViewById(R.id.add_class_button);
+        name = findViewById(R.id.addClassName);
+        room = findViewById(R.id.addClassRoom);
+        teacher = findViewById(R.id.addClassTeacher);
+        final TimePicker time = findViewById(R.id.addClassTime);
+
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Do something in response to button click
-                String taskId = mUserTasks.push().getKey();
+                String classId = mUserClasses.push().getKey();
                 // creating user object
-                Tasks task = new Tasks(name.getText().toString(), String.valueOf(duedate.getDayOfMonth()+"/"+duedate.getMonth()+"/"+duedate.getYear()), "false", clas.getText().toString());
+                Classes classes = new Classes(name.getText().toString(), room.getText().toString(), teacher.getText().toString(),String.valueOf(time.getHour()+":"+time.getMinute()));
 
-                mUserTasks.child(taskId).setValue(task);
+                mUserClasses.child(classId).setValue(classes);
 
                 finish();
             }
