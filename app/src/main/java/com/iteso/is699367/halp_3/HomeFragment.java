6c49fde1a380243b910dfe308d5f86ddcb566d05
Jorge Perez <alberto.perezf2@gmail.com>
2018-05-14 05:17:35 -0500

    added all

--- app/src/main/java/com/iteso/is699367/halp_3/HomeFragment.java
+++ app/src/main/java/com/iteso/is699367/halp_3/HomeFragment.java
@@ -18,6 +18,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.iteso.is699367.halp_3.beans.Classes;
 import com.iteso.is699367.halp_3.beans.Tasks;
 import com.iteso.is699367.halp_3.database.DataBaseHandler;
 
@@ -25,10 +26,13 @@ import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
     RecyclerView recyclerView;
+    RecyclerView recyclerView1;
     DatabaseReference firebaseDatabase;
     DatabaseReference mUserTasks;
+    DatabaseReference mUserClasses;
     FirebaseAuth mAuth;
     ArrayList<Tasks> tasks = new ArrayList<Tasks>();
+    ArrayList<Classes> classes = new ArrayList<Classes>();
 
     public HomeFragment() {
         // Required empty public constructor
@@ -41,10 +45,12 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
         recyclerView = rootView.findViewById(R.id.fragment_recycler);
+        recyclerView1 = rootView.findViewById(R.id.fragment_recycler1);
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser user = mAuth.getCurrentUser();
         firebaseDatabase = FirebaseDatabase.getInstance().getReference();
         mUserTasks = firebaseDatabase.child("users").child(user.getUid()).child("assignments");
+        mUserClasses = firebaseDatabase.child("users").child(user.getUid()).child("classes");
         return rootView;
     }
 
@@ -57,12 +63,6 @@ public class HomeFragment extends Fragment {
         // Use a linear layout manager
         LinearLayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(mLayoutManager);
-
-        //TODO: fill in task array goes here
-
-
-        Log.i("COUNT ARRAY LIST0", String.valueOf(tasks.size()));
-
         mUserTasks.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -76,6 +76,29 @@ public class HomeFragment extends Fragment {
                 Log.i("COUNT ARRAY LIST", String.valueOf(tasks.size()));
                 AdapterTasks adapterProduct = new AdapterTasks(0, getActivity(), tasks);
                 recyclerView.setAdapter(adapterProduct);
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+
+        recyclerView1.setHasFixedSize(true);
+        // Use a linear layout manager
+        LinearLayoutManager mLayoutManager1 = new LinearLayoutManager(getActivity());
+        recyclerView1.setLayoutManager(mLayoutManager1);
+        mUserClasses.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                //User value = dataSnapshot.getValue(User.class);
+                //message.setText(value.toString());
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    Classes value = snapshot.getValue(Classes.class);
+                    Log.d("FIREBASE", "Key is: " + snapshot.getKey() + " Value is: " + snapshot.getValue());
+                    classes.add(value);
+                }
+                Log.i("COUNT ARRAY LIST", String.valueOf(classes.size()));
+                AdapterClasses adapterProduct1 = new AdapterClasses(0, getActivity(), classes);
+                recyclerView1.setAdapter(adapterProduct1);
             }
             @Override
             public void onCancelled(DatabaseError databaseError) {
